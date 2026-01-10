6589ff418d650370cb0572ea86f00bb1d93f5cb6
nearlynithin <nearlynithin@gmail.com>
2026-01-10 06:30:32 +0530

    refac: scroll setup + Demo 3 page scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import Scene from "./scenes/scene";
+import Experience from "./scenes/Experience";
+import { ScrollControls, Stats } from "@react-three/drei";
 
 function App() {
   return (
     <>
-      <div id="scroll-spacer" />
-
       <div className="fixed inset-0 z-10 pointer-events-none">
-        <Canvas>
-          <Scene />
+        <Canvas camera={{ position: [0, 0, 20], fov: 45, near: 0.1, far: 5000 }} >
+          <Stats />
+          <ScrollControls pages={3} damping={0.4} distance={1} >
+            <Experience />
+          </ScrollControls>
         </Canvas>
       </div>
-
       <div className="fixed left-0 right-0 bottom-[12vh] z-20 flex justify-center pointer-events-none">
         <div className="max-w-[95vw] overflow-hidden">
           <h2
-            className="text-white whitespace-nowrap comic-sans font-bold"
+            className="text-white whitespace-nowrap font-bold"
             style={{ fontSize: "clamp(1.0rem, 4vw, 2.0rem)" }}
           >
             COMING SOON !
--- src/assets/Wall.tsx
+++ src/assets/Wall.tsx
@@ -8,14 +8,12 @@ type WallProps = {
 export function Wall({ size }: WallProps) {
 
   const positions = [
-    new THREE.Vector3(0, 0, -40),
-    new THREE.Vector3(1, 0, -40),
-    new THREE.Vector3(-1, 0, -40),
+    new THREE.Vector3(0, 0, -20),
+    new THREE.Vector3(1, 0, -20),
+    new THREE.Vector3(-1, 0, -20),
   ]
-
-  const wallSize = (size < 6) ? { x: 10, y: 35 } : { x: 25, y: 35 };
-  const geometry = useMemo(() => new THREE.PlaneGeometry(wallSize.x, wallSize.y), []);
-  const defaultMaterial = useMemo(() => new THREE.MeshStandardMaterial({ color: 0x888888 }), []);
+  const wallSize = (size < 6) ? { x: 10, y: 55 } : { x: 25, y: 35 };
+  const geometry = useMemo(() => new THREE.PlaneGeometry(wallSize.x, wallSize.y), [wallSize]);
 
   return (
     <>
