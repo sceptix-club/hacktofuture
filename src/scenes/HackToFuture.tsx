3bf5e7c1c4ebe050569da08919b5ff2189be1d7c
nearlynithin <nearlynithin@gmail.com>
2026-02-05 19:59:27 +0530

    feat: outlines for HackToFuture title

--- src/scenes/HackToFuture.tsx
+++ src/scenes/HackToFuture.tsx
@@ -9,16 +9,48 @@ const HackToFuture = ({ viewportWidth, progress }: HtfProps) => {
   const font = "/fonts/DelaGothicOne-Regular.ttf";
 
   return (
-    <Text
-      font={font}
-      position={[0, 0, 5 + 10 * progress]}
-      fontSize={viewportWidth < 15 ? 0.5 : 1.0}
-      color="white"
-      anchorX="center"
-      anchorY="middle"
-    >
-      HACKTOFUTURE
-    </Text>
+    <>
+      <Text
+        font={font}
+        position={[0, 0, 5 + 10 * progress]}
+        fontSize={viewportWidth < 15 ? 0.5 : 1.0}
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+        fillOpacity={0.3}
+        outlineWidth={0.02}
+        outlineColor="black"
+        outlineOpacity={0.5}
+      >
+        HACKTOFUTURE
+      </Text>
+
+      <Text
+        font={font}
+        position={[0, 0, 5 + 10 * progress]}
+        fontSize={viewportWidth < 15 ? 0.5 : 1.0}
+        color="grey"
+        anchorX="center"
+        anchorY="middle"
+        fillOpacity={0.1}
+        outlineWidth={0.04}
+        outlineColor="grey"
+        outlineOpacity={0.3}
+      >
+        HACKTOFUTURE
+      </Text>
+
+      <Text
+        font={font}
+        position={[0, 0, 5 + 10 * progress]}
+        fontSize={viewportWidth < 15 ? 0.5 : 1.0}
+        color="white"
+        anchorX="center"
+        anchorY="middle"
+      >
+        HACKTOFUTURE
+      </Text>
+    </>
   );
 };
 
